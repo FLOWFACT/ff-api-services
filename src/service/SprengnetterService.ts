2363a4eb3d5a8d54012dfa46f1ef2898111b7411
ffbesnikcelaj <besnik.celaj@flowfact.de>
2019-06-28 08:00:04 +0200

    Adding ressources to sprengnetter service

--- src/service/SprengnetterService.ts
+++ src/service/SprengnetterService.ts
@@ -39,6 +39,24 @@ export class SprengnetterService extends APIClient {
         return await this.invokeApi('/api/rent', 'POST', body);
     }
 
+    /**
+     * Fetches all inquiries with pagination support
+     * @param {String} entityId - The entityId of the estate that should the htmlView should be created for.
+     * @returns {Promise<AxiosResponse>} The response contains the html that represents the marketanalysis.
+     */
+    async fetchHtml(entityId: string): Promise<AxiosResponse> {
+        return await this.invokeApi('/htmlView/' + entityId);
+    }
+
+    /**
+     * Fetches all inquiries with pagination support
+     * @param {String} entityId - The entityId of the estate that should the pdfView should be created for.
+     * @returns {Promise<AxiosResponse>} The response contains a link to the pdf that is stored on S3.
+     */
+    async fetchPdfLink(entityId: String): Promise<AxiosResponse> {
+        return await this.invokeApi('/pdfView/' + entityId);
+    }
+
 }
 
 export default new SprengnetterService();
